1ef9fddb8258f1c4058e6e84ff09551f56e04d57
Matevz Vucnik <vucnikm@gmail.com>
2016-07-28 10:50:56 +0200

    fix

--- sunseed.py
+++ sunseed.py
@@ -1,58 +1,31 @@
 #!/usr/bin/env python
 
 import sys
-import time
-import json
-from datetime import datetime
+import socket
 from videk_rest_client import Videk
-from random import randint
 
-x = Videk('url', 'secret')
+videk = Videk("address", "secret")
 
-cluster = "example";
-node = "example.com"
-sensor_t = "test"
-sensor_q = "test_value"
-sensor_u = "t"
+if videk.serverOn:
+    echo "Videk server is online ..."
+else:
+    echo "Videk server is offline ..."
+    sys.exit(1)
+
+node = "node-name"
+cluster = "cluster-name"
 lat = 46.042767
 lon = 14.487632
 
-x.latitude = lat
-x.longitude = lon
+videk.latitude = lat
+videk.longitude = lon
 
-cluster_id = x.getClusterID(cluster)
+cluster_id = videk.getClusterID(cluster)
 if cluster_id == None:
-    x.createCluster(cluster)
-    cluster_id = x.getClusterID(cluster)
+    videk.createCluster(cluster)
+    cluster_id = videk.getClusterID(cluster)
 
-node_id = x.getNodeID(node)
+node_id = videk.getNodeID(node)
 if node_id == None:
-    x.createNode(node, cluster_id)
-    node_id = x.getNodeID(node)
-
-sensor_id = x.getSensorID(node, sensor_t, sensor_q)
-if sensor_id == None:
-    x.createSensor(node_id, sensor_t, sensor_q, sensor_u)
-    sensor_id = x.getSensorID(node, sensor_t, sensor_q)
-
-measurement = '''{"latitude":"","longitude":"","ts":"","value":""}'''
-
-while True:
-    try:
-        v = randint(1, 10)
-
-        preparedData = []
-        data = json.loads(measurement)
-        data['value'] = v
-        data['ts'] = datetime.utcnow().isoformat()
-        data['latitude'] = lat
-        data['longitude'] = lon
-        preparedData.append(data)
-
-        x.uploadMesurements(preparedData, node_id, sensor_id)
-
-        time.sleep(10)
-    except (KeyboardInterrupt, SystemExit):
-         sys.exit(0)
-    except:
-         time.sleep(10)
+    videk.createNode(node, cluster_id)
+    node_id = videk.getNodeID(node)
