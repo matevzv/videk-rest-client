34b0ffc0c2a588f76a2781c9bfdb79d97c282216
Matevz Vucnik <vucnikm@gmail.com>
2016-09-16 10:37:36 +0200

    add HW id support

--- sunseed.py
+++ sunseed.py
@@ -16,6 +16,7 @@ node = "node-name"
 cluster = "cluster-name"
 lat = 46.042767
 lon = 14.487632
+hw_id = open("/etc/machine-id", "r").readline().rstrip()
 
 videk.latitude = lat
 videk.longitude = lon
@@ -29,3 +30,4 @@ node_id = videk.getNodeID(node)
 if node_id == None:
     videk.createNode(node, cluster_id)
     node_id = videk.getNodeID(node)
+    videk.addNodeExtraField(node_id, "HW_ID", hw_id)
