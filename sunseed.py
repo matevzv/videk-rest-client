0e0f1bba72c1310ca91537d9810983419a642d84
Matevz Vucnik <vucnikm@gmail.com>
2016-09-16 17:19:43 +0200

    add sensor and data support

--- sunseed.py
+++ sunseed.py
@@ -2,9 +2,10 @@
 
 import sys
 import socket
+import os.path
 from videk_rest_client import Videk
 
-videk = Videk("address", "secret")
+videk = Videk("http://localhost:3000", "vMrr4jELLU1vxL9SdY63qJtlEZq42ykz")
 
 if videk.serverOnline():
     print "Videk server is online ..."
@@ -17,6 +18,9 @@ cluster = "cluster-name"
 lat = 46.042767
 lon = 14.487632
 hw_id = open("/etc/machine-id", "r").readline().strip()
+
+pmc_file_name = "/tmp/pmc-data"
+spm_file_name = "/tmp/spm-data"
 
 videk.latitude = lat
 videk.longitude = lon
@@ -31,3 +35,12 @@ if node_id == None:
     videk.createNode(node, cluster_id)
     node_id = videk.getNodeID(node)
     videk.addNodeExtraField(node_id, "HW_ID", hw_id)
+
+if os.path.isfile(pmc_file_name):
+    pmc = open(pmc_file_name, "r").readline().strip()
+    pmc = json.loads(pmc)
+    # TODO register each sensor and upload data
+elif os.path.isfile(spm_file_name):
+    print "read spm data"
+else:
+    sys.exit(1)
